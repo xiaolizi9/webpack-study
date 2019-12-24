2eb6aea91479916c8ff0ef65d9d99fd91f8c289a
栗艳宁 <2397394268@qq.com>
2019-12-24 15:29:39 +0800

    2.修改webpack的配置文件名

--- webpack.config.my.js
+++ webpack.config.my.js
@@ -5,6 +5,6 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build')  //必须为绝对路径
+        path: path.resolve(__dirname, 'dist')  //必须为绝对路径
     }
 }
