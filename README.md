7ab482240bc30f5418aed04f29b1ad6918486a05
栗艳宁 <2397394268@qq.com>
2019-12-24 14:54:12 +0800

    1.webpack的基本配置

---
+++ README.md
@@ -0,0 +1,2 @@
+## 安装webpack-cli -D表示是开发依赖
++ npm i webpack-cli -D
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',    // 模式,分为development和production模式
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')  //必须为绝对路径
+    }
+}
