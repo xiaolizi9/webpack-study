b2d90fbb18dbdd0a72c29bdc93e6953fc6fca35c
栗艳宁 <2397394268@qq.com>
2020-01-03 11:23:20 +0800

    第23节

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './index.css'
 
 // 此处的window.root表示index.html中的<div id="root"></div>
 render(<h1>我是reactDOM渲染的</h1>, window.root)
