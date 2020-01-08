cf0b6129f4577176a4d5b0b9c441e96a9e839ff0
栗艳宁 <2397394268@qq.com>
2020-01-08 10:34:20 +0800

    手写loader

--- src/source.js
+++
@@ -1,1 +0,0 @@
-export default '我是要热更新的文件'
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,30 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const WebpackCleanPlugin = require('webpack-clean-plugin')
-const webpack = require('webpack')
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
-    devtool: 'source-map',
-    devServer: {
-        open: true,
-        // 显示进度条
-        progress: true,
-        // 启用热更新功能
-        hot: true
-    },
     plugins: [
-        new WebpackCleanPlugin(),
         new HtmlWebpackPlugin({
+            filename: 'index.html',
             template: './public/index.html'
-        }),
-        // 热更新模块
-        new webpack.HotModuleReplacementPlugin(),
-        // 这个模块可以告诉我们是哪个文件发生了变化
-        new webpack.NamedModulesPlugin()
+        })
     ],
     module: {
         rules: [
             {
-                test: /\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/preset-react'
-                        ]
+                test: /\.js/,
+                use: [
+                    {
+                        loader:'./my-loader/bind-to-on-loader.js'
                     }
-                }
+                ]
             }
         ]
     }
